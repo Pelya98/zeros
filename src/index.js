d7dba5c231530422763c7ed70b66473c18f4ebf1
Егор Пилипенко <egor.pilipenko.1998@gmail.com>
2018-02-22 20:02:40 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+module.exports =function getZerosCount(number) {
+  let result=0;
+  let delitel=5;
+  for(let i=0; i<11;i++){
+      let dop=number/delitel;
+      dop=Math.floor(dop);
+      result+=dop;
+      delitel*=5;
+  }
+
+  return result;
+};
